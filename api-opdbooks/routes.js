f9736b976027ebfa934a2c0afda1218f8e880012
Jidapa Sikaphan <jidapa40@hotmail.com>
2018-03-17 01:12:03 +0700

    [CREATE] AddPateint API

---
+++ api-opdbooks/routes.js
@@ -0,0 +1,62 @@
+const express = require('express')
+var router = express.Router();
+
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: 'localhost',
+        user: 'root',
+        password: '020540',
+        database: 'TestHelloworld'
+    }
+});
+
+router.get('/', async (req, res) => {
+    var data = await knex.table('PateintRecords')
+        .select()
+    res.send(data);
+})
+
+router.post('/addPateint', async function (req, res) {
+    console.log(req.body)
+    var data = await knex.table('PateintRecords')
+        .insert({
+            hospitalNumber: req.body.hospitalNumber,
+            nameTitleTH: req.body.nameTitleTH,
+            firstnameTH: req.body.firstnameTH,
+            lastnameTH: req.body.lastnameTH,
+            nameTitleEN: req.body.nameTitleEN,
+            firstnameEN: req.body.firstnameEN,
+            lastnameEN: req.body.lastnameEN,
+            gender: req.body.gender,
+            dob: req.body.dob,
+            occupartion: req.body.occupartion,
+            idcard: req.body.idcard,
+            bloodGroup: req.body.bloodGroup,
+            nationality: req.body.nationality,
+            religion: req.body.religion,
+            status: req.body.status,
+            homePhonenumber: req.body.homePhonenumber,
+            mobileNumber: req.body.mobileNumber,
+            nameFather: req.body.nameFather,
+            nameMother: req.body.nameMother,
+            typeofHouse: req.body.typeofHouse,
+            address: req.body.address,
+            province: req.body.province,
+            district: req.body.district,
+            subDistrict: req.body.subDistrict,
+            zipcode: req.body.zipcode,
+            emerName: req.body.emerName,
+            emerRelationship: req.body.emerRelationship,
+            emerHomePhonenumber: req.body.emerHomePhonenumber,
+            emerMobileNumber: req.body.emerMobileNumber,
+            emerAddress: req.body.emerAddress,
+            privilege: req.body.privilege,
+            allergy: req.body.allergy,
+            congenitalDisease: req.body.congenitalDisease,
+        })
+
+    res.end('ADD Success');
+});
+
+module.exports = router
---
+++ api-opdbooks/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const routes = require('./routes')
+
+app.use('/', routes)
+
+app.listen(3003, () => {
+    console.log('Start server at port 3003.')
+})
