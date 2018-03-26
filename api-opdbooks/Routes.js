57105fb1cca2d36591f23400a3ed3ea59df2e0f2
Jidapa Sikaphan <jidapa40@hotmail.com>
2018-03-27 05:38:51 +0700

    [UPDATE] Validate idCard API in Routes.js

--- api-opdbooks/Routes.js
+++ api-opdbooks/Routes.js
@@ -12,115 +12,124 @@ var knex = require('knex')({
 });
 
 router.post('/addPateint', async function (req, res) {
-    var maxHN = await knex.table('PateintRecords')
-                .select()
-                .max('hospitalNumber as maxHN')
-    var hospitalNumber = (maxHN[0].maxHN)+1
-    var data = await knex.table('PateintRecords')
-        .insert({
-            hospitalNumber: hospitalNumber,
-            registerDate: req.body.registerDate,
-            idCard: req.body.idCard,
-            nameTitleTH: req.body.nameTitleTH,
-            firstnameTH: req.body.firstnameTH,
-            lastnameTH: req.body.lastnameTH,
-            nameTitleEN: req.body.nameTitleEN,
-            firstnameEN: req.body.firstnameEN,
-            lastnameEN: req.body.lastnameEN,
-            gender: req.body.gender,
-            dob: req.body.dob,
-            bloodgroup: req.body.bloodgroup,
-            nationality: req.body.nationality,
-            religion: req.body.religion,
-            status: req.body.status,
-            occupartion: req.body.occupartion,
-            homePhonenumber: req.body.homePhonenumber,
-            mobileNumber: req.body.mobileNumber,
-            congenitalDisease: req.body.congenitalDisease,
-            typeofHouse: req.body.typeofHouse,
-            address: req.body.address,
-            province: req.body.province,
-            district: req.body.district,
-            subDistrict: req.body.subDistrict,
-            zipcode: req.body.zipcode,
-            emerTitle: req.body.emerTitle,
-            emerFirstname: req.body.emerFirstname,
-            emerLastname: req.body.emerLastname,
-            emerRelationship: req.body.emerRelationship,
-            emerHomePhonenumber: req.body.emerHomePhonenumber,
-            emerMobileNumber: req.body.emerMobileNumber,
-            emerTypeofHouse: req.body.emerTypeofHouse,
-            emerAddress: req.body.emerAddress,
-            emerProvince: req.body.emerProvince,
-            emerDistrict: req.body.emerDistrict,
-            emerSubDistrict: req.body.emerSubDistrict,
-            emerZipcode: req.body.emerZipcode,
-            statusSameAddress: req.body.statusSameAddress,
-            fatherFirstname: req.body.fatherFirstname,
-            fatherLastname: req.body.fatherLastname,
-            motherFirstname: req.body.motherFirstname,
-            motherLastname: req.body.motherLastname,
-            allergy: req.body.allergy,
-            privilege: req.body.privilege,
+    var idCards = await knex.table('PateintRecords')
+        .select('idcard')
+        .where('idcard', req.body.idCard)
+    if (idCards === null || idCards.length === 0){
+        var maxHN = await knex.table('PateintRecords')
+            .select()
+            .max('hospitalNumber as maxHN')
+        var hospitalNumber = (maxHN[0].maxHN) + 1
+        var data = await knex.table('PateintRecords')
+            .insert({
+                hospitalNumber: hospitalNumber,
+                registerDate: req.body.registerDate,
+                idCard: req.body.idCard,
+                nameTitleTH: req.body.nameTitleTH,
+                firstnameTH: req.body.firstnameTH,
+                lastnameTH: req.body.lastnameTH,
+                nameTitleEN: req.body.nameTitleEN,
+                firstnameEN: req.body.firstnameEN,
+                lastnameEN: req.body.lastnameEN,
+                gender: req.body.gender,
+                dob: req.body.dob,
+                bloodgroup: req.body.bloodgroup,
+                nationality: req.body.nationality,
+                religion: req.body.religion,
+                status: req.body.status,
+                occupartion: req.body.occupartion,
+                homePhonenumber: req.body.homePhonenumber,
+                mobileNumber: req.body.mobileNumber,
+                congenitalDisease: req.body.congenitalDisease,
+                typeofHouse: req.body.typeofHouse,
+                address: req.body.address,
+                province: req.body.province,
+                district: req.body.district,
+                subDistrict: req.body.subDistrict,
+                zipcode: req.body.zipcode,
+                emerTitle: req.body.emerTitle,
+                emerFirstname: req.body.emerFirstname,
+                emerLastname: req.body.emerLastname,
+                emerRelationship: req.body.emerRelationship,
+                emerHomePhonenumber: req.body.emerHomePhonenumber,
+                emerMobileNumber: req.body.emerMobileNumber,
+                emerTypeofHouse: req.body.emerTypeofHouse,
+                emerAddress: req.body.emerAddress,
+                emerProvince: req.body.emerProvince,
+                emerDistrict: req.body.emerDistrict,
+                emerSubDistrict: req.body.emerSubDistrict,
+                emerZipcode: req.body.emerZipcode,
+                statusSameAddress: req.body.statusSameAddress,
+                fatherFirstname: req.body.fatherFirstname,
+                fatherLastname: req.body.fatherLastname,
+                motherFirstname: req.body.motherFirstname,
+                motherLastname: req.body.motherLastname,
+                allergy: req.body.allergy,
+                privilege: req.body.privilege,
 
 
-            nameTitleTH: req.body.nameTitleTH,
-            firstnameTH: req.body.firstnameTH,
-            lastnameTH: req.body.lastnameTH,
-            nameTitleEN: req.body.nameTitleEN,
-            firstnameEN: req.body.firstnameEN,
-            lastnameEN: req.body.lastnameEN,
-            gender: req.body.gender,
-            dob: req.body.dob,
-            occupartion: req.body.occupartion,
-            idCard: req.body.idCard,
-            bloodgroup: req.body.bloodgroup,
-            nationality: req.body.nationality,
-            religion: req.body.religion,
-            status: req.body.status,
-            homePhonenumber: req.body.homePhonenumber,
-            mobileNumber: req.body.mobileNumber,
-            congenitalDisease: req.body.congenitalDisease,
+                nameTitleTH: req.body.nameTitleTH,
+                firstnameTH: req.body.firstnameTH,
+                lastnameTH: req.body.lastnameTH,
+                nameTitleEN: req.body.nameTitleEN,
+                firstnameEN: req.body.firstnameEN,
+                lastnameEN: req.body.lastnameEN,
+                gender: req.body.gender,
+                dob: req.body.dob,
+                occupartion: req.body.occupartion,
+                idCard: req.body.idCard,
+                bloodgroup: req.body.bloodgroup,
+                nationality: req.body.nationality,
+                religion: req.body.religion,
+                status: req.body.status,
+                homePhonenumber: req.body.homePhonenumber,
+                mobileNumber: req.body.mobileNumber,
+                congenitalDisease: req.body.congenitalDisease,
 
-            fatherFirstname: req.body.fatherFirstname,
-            fatherLastname: req.body.fatherLastname,
-            motherFirstname: req.body.motherFirstname,
-            motherLastname: req.body.motherLastname,
+                fatherFirstname: req.body.fatherFirstname,
+                fatherLastname: req.body.fatherLastname,
+                motherFirstname: req.body.motherFirstname,
+                motherLastname: req.body.motherLastname,
 
-            typeofHouse: req.body.typeofHouse,
-            address: req.body.address,
-            province: req.body.province,
-            district: req.body.district,
-            subDistrict: req.body.subDistrict,
-            zipcode: req.body.zipcode,
+                typeofHouse: req.body.typeofHouse,
+                address: req.body.address,
+                province: req.body.province,
+                district: req.body.district,
+                subDistrict: req.body.subDistrict,
+                zipcode: req.body.zipcode,
 
-            emerTitle: req.body.emerTitle,
-            emerFirstname: req.body.emerFirstname,
-            emerLastname: req.body.emerLastname,
-            emerRelationship: req.body.emerRelationship,
-            emerHomePhonenumber: req.body.emerHomePhonenumber,
-            emerMobileNumber: req.body.emerMobileNumber,
-            emerTypeofHouse: req.body.emerTypeofHouse,
-            emerAddress: req.body.emerAddress,
-            emerProvince: req.body.emerProvince,
-            emerDistrict: req.body.emerDistrict,
-            emerSubDistrict: req.body.emerSubDistrict,
-            emerZipcode: req.body.emerZipcode,
+                emerTitle: req.body.emerTitle,
+                emerFirstname: req.body.emerFirstname,
+                emerLastname: req.body.emerLastname,
+                emerRelationship: req.body.emerRelationship,
+                emerHomePhonenumber: req.body.emerHomePhonenumber,
+                emerMobileNumber: req.body.emerMobileNumber,
+                emerTypeofHouse: req.body.emerTypeofHouse,
+                emerAddress: req.body.emerAddress,
+                emerProvince: req.body.emerProvince,
+                emerDistrict: req.body.emerDistrict,
+                emerSubDistrict: req.body.emerSubDistrict,
+                emerZipcode: req.body.emerZipcode,
 
-            privilege: req.body.privilege,
-            allergy: req.body.allergy,
+                privilege: req.body.privilege,
+                allergy: req.body.allergy,
 
-            statusSameAddress: req.body.statusSameAddress
-        })
-    console.log(data)
-    console.log('Success')
-    res.end('ADD Success');
+                statusSameAddress: req.body.statusSameAddress
+            })
+        console.log(data)
+        console.log('Success')
+        res.end('ADD Success');
+    }else{
+        res.end('ซ้ำ');
+    }
+
+   
 });
 
 router.get('/provinceDB', async (req, res) => {
     var data = await knex.table('provinces')
         .select()
-        console.log('province')
+    console.log('province')
     res.send(data);
 })
 
@@ -190,7 +199,7 @@ router.get('/province', async (req, res) => {
     const data = provinces
         .map(province => ({
             key: province.PROVINCE_ID,
-            text: province.PROVINCE_NAME + ' (' + province.PROVINCE_NAME_ENG+')',
+            text: province.PROVINCE_NAME + ' (' + province.PROVINCE_NAME_ENG + ')',
             value: province.PROVINCE_NAME + ' (' + province.PROVINCE_NAME_ENG + ')'
         }))
         .sort((a, b) => a.text.localeCompare(b.text))
@@ -226,5 +235,20 @@ router.get('/district', async (req, res) => {
         .sort((a, b) => a.text.localeCompare(b.text))
     res.send(data);
 })
+
+router.get('/validateIdcard', async (req, res) => {
+    var data = await knex.table('PateintRecords')
+        .select('idcard')
+    // .where('idcard',idcard)
+    res.send(data);
+})
+router.get('/validateIdcard/:id', async (req, res) => {
+    var data = await knex.table('PateintRecords')
+        .select('idcard')
+        .where('idcard', req.params.id)
+    res.send(data);
+})
+
+
 
 module.exports = router
