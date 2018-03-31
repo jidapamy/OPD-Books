1c9535c51d8c737a90ae1e32b95103a27d48dccf
stampstood <stamp-chu.min@hotmail.com>
2018-04-01 03:31:28 +0700

    [CREATE] upload profile picture in register.js

--- site-opdbooks/src/Register/HeaderForm.js
+++ site-opdbooks/src/Register/HeaderForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Header, Icon, Image, Grid } from 'semantic-ui-react'
+import Basic from './img/Upload.js';
 
 const GridColumn = styled(Grid.Column) `
     display: flex;
@@ -17,11 +18,13 @@ const HeaderExampleUsersIcon = () => (
             </Header>
       <Header as='h2' textAlign='center'>
       <Header.Content>
-        
+      <p>รูปประจำตัว</p>
       </Header.Content>
     
+     {/* <GridColumn ><Image src='https://react.semantic-ui.com/assets/images/avatar/large/stevie.jpg' size='small' circular /></GridColumn> */}
+     <Basic>
      <GridColumn ><Image src='https://react.semantic-ui.com/assets/images/avatar/large/stevie.jpg' size='small' circular /></GridColumn>
-      
+     </Basic>
     </Header>
     <br></br> <br></br> 
   </div>
