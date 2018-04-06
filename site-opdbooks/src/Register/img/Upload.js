5ebe1b07e3ceff9a0756c91d00149631e8d232aa
stampstood <stamp-chu.min@hotmail.com>
2018-04-06 18:19:04 +0700

    [UPDATE] clean code

--- site-opdbooks/src/Register/img/Upload.js
+++ site-opdbooks/src/Register/img/Upload.js
@@ -20,31 +20,18 @@ const GridColumns = styled(Dropzone) `
 `
 
 
-
-
 export default class Basic extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            file: [],
-            imagePreviewUrl: '',
+    imageDefault = 'https://react.semantic-ui.com/assets/images/avatar/large/stevie.jpg'
+    state = {
+        file: [],
+        imagePreviewUrl: '',
+    };
 
-
-
-        };
-        this._handleImageChange = this._handleImageChange.bind(this);
-        this._handleSubmit = this._handleSubmit.bind(this);
-    }
-
-    _handleSubmit(e) {
+    handleSubmit = (e) => {
         e.preventDefault();
     }
 
-    _handleSubmit(e) {
-        e.preventDefault();
-    }
-
-    _handleImageChange(e) {
+    handleImageChange = (e) => {
         e.preventDefault();
 
         let reader = new FileReader();
@@ -55,22 +42,17 @@ export default class Basic extends React.Component {
                 file: file,
                 imagePreviewUrl: reader.result
             });
+            console.log( reader.result)
         }
-        // console.log(file);
+        console.log(file);
         reader.readAsDataURL(file)
-    }
-
-    getFile(files) {
-        this.setState({files}),
-        console.log(files);
     }
 
     render() {
         let { imagePreviewUrl } = this.state;
-        let $imagePreview = <Image src='https://react.semantic-ui.com/assets/images/avatar/large/stevie.jpg' size='small' circular />;
+        let $imagePreview =  <Image src='https://react.semantic-ui.com/assets/images/avatar/large/stevie.jpg' size='small' circular />;
         if (imagePreviewUrl) {
             $imagePreview = (<img src={imagePreviewUrl} />);
-
         }
 
         return (
@@ -78,21 +60,16 @@ export default class Basic extends React.Component {
                 <div className="DivPicCenter">
                     <GridColumns type="file" onChange={this._handleImageChange}>
                         <Image className="circular" circular>
-                            <div className="setImage">{$imagePreview}</div>
+                            <div className="setImage">
+                             <Image src={this.state.imagePreviewUrl === '' ?  this.imageDefault : this.state.imagePreviewUrl} size='small' circular />
+                            </div>
                         </Image>
                         <div className="IconCamera">
                             <Image src={cameraIcon} />
                         </div>
-
                     </GridColumns>
-                    <FileBase64
-                        multiple={true}
-                        onDone={this.getFile.bind(this)} 
-                        />
-
                 </div>
                 <br />
-
 
 
             </section>
