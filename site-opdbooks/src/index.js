7a3cdee00a9519c902d30faea557733df4be9649
Jidapa Sikaphan <jidapa40@hotmail.com>
2018-04-08 22:11:31 +0700

    [UPDATE] change route frontend

--- site-opdbooks/src/index.js
+++ site-opdbooks/src/index.js
@@ -6,7 +6,7 @@ import Home from './BetaIndex/Home';
 import BetaRegis from './BetaRegis/BetaRegister';
 import Test from './Test';
 import Basic from './Register/img/Upload';
-import Register from './Register/Register';
+// import Register from './Register/Register';
 import Register2 from './Containers/Register';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-datepicker/dist/react-datepicker.css';
@@ -17,14 +17,14 @@ ReactDOM.render(
     <Router>
         <div>
         <Route exact path='/' component={Home}/>
-        <Route path='/register' component={Register}/>
+        {/* <Route path='/register' component={Register}/> */}
         <Route path='/login' component={Login} />
-        <Route path='/home' component={Home} />
-        <Route path='/test' component={Test} />
-        <Route path='/img' component={Basic} />
-        <Route path='/betaregister' component={BetaRegis} />
+        {/* <Route path='/home' component={Home} /> */}
+        {/* <Route path='/test' component={Test} /> */}
+        {/* <Route path='/img' component={Basic} /> */}
+        {/* <Route path='/betaregister' component={BetaRegis} /> */}
         
-        <Route path='/register2' component={Register2} />
+        <Route path='/register' component={Register2} />
         </div>
     </Router>,document.getElementById('root')
     );
