5cd85a62bdbacff9fc99c10ed6ca0a25ed5ba8c2
Jidapa Sikaphan <jidapa40@hotmail.com>
2018-09-20 22:50:41 +0700

    [update] gitignore

--- projects/src/index.js
+++ projects/src/index.js
@@ -10,27 +10,31 @@ import ReactDOM from 'react-dom';
 // import PatientTreatment from "./Employee/Containers/PatientTreatment";
 // import EmpLogin from "./Employee/Containers/EmpLogin";
 // import MedicalRecordTreatment from "./Employee/Containers/MedicalRecordTreatment";
-// import Stamp from "./stamp/App";
 
 
 
 import Home from "./Home";
 import Login from "./Patients/Containers/Login";
 import ManagePatientRecord from "./Patients/Containers/ManagePatientRecord";
-// import ErrorNotFound from "./Error/ErrorNotFound";
-// import ErrorTimeOut from "./Error/ErrorTimeOut";
 import ProfilePatient from "./Patients/Containers/PatientProfile";
+// import ProfilePatient from "./containers/patientProfile";
+
 // import EmpLogin from "./Employees/Containers/EmpLogin";
 import EmpTest from "./Employees/Containers/EmpTest";
-// import Registration from "./Employees/Containers/Registration";
+import Registration from "./Employees/Containers/Registration";
 // import PatientTreatment from "./Employees/Containers/PatientTreatment";
 // import MedicalRecordTreatment from "./Employees/Containers/MedicalRecordTreatment";
-
-import APIHome from "./APIdata/Containers/APIHome"
+// import NavbarQueses from "./Employees/Components/NavbarQueses"
+import apiDocument from "./Employees/Components/apiDoc"
+// import Testt from "./Employees/Components/Test"
+// import DemoExample from "./containers/demoExample"
 
 
 import 'semantic-ui-css/semantic.min.css';
 import 'react-datepicker/dist/react-datepicker.css';
+
+
+
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -45,19 +49,19 @@ ReactDOM.render(
 
       {/* emp */}
       <Route path="/empTest" component={EmpTest} />
-      <Route path="/api" component={APIHome} />
+      {/* <Route path="/api" component={DemoExample} /> */}
       
       {/* <Route path="/signinForEmployee" component={EmpLogin} /> */}
-      {/* <Route path="/Registration" component={Registration} />*/} 
+      <Route path="/Registration" component={Registration} /> 
       {/* <Route path="/MedicalRecordTreatment" component={MedicalRecordTreatment} />  */}
       {/* <Route path="/employeeSegment" component={EmployeeSegment} /> */}
       {/* <Route path="/patientTreatment" component={PatientTreatment} /> */}
 
       {/* <Route path="/stamp" component={Stamp} />
-      <Route path="/testMedicalRecord" component={MedicalRecordTreatment} /> */}
-      {/* Error */}
-      {/* <Route path="/ErrorNotFound" component={ErrorNotFound} />
-      <Route path="/ErrorTimeOut" component={ErrorTimeOut} /> */}
+      <Route path="/testMedicalRecord" component={MedicalRecordTreatment} />
+      <Route path="/test" component={NavbarQueses} /> */}
+      <Route path="/apiDoc" component={apiDocument} />
+      {/* <Route path="/test2" component={Testt} /> */}
     </div>
   </Router>,
   document.getElementById("root")
