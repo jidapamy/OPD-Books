9e195e76c29ee3f1b3889de34b5b685fdb043539
Jidapa Sikaphan <jidapa40@hotmail.com>
2018-09-22 20:30:07 +0700

    [UPDATE] index.js

--- projects/src/index.js
+++ projects/src/index.js
@@ -1,36 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import Home from './Patient/Containers/Home';
-// import ManagePatientRecord from './Patient/Containers/ManagePatientRecord';
-// import Login from './Patient/Containers/Login';
-// import ProfilePatient from './Patient/Containers/PatientProfile';
-// import Registration from './Patient/Containers/Registration';
-// import EmployeeSegment from './Employee/Containers/EmployeeSegment';
-// import PatientTreatment from "./Employee/Containers/PatientTreatment";
-// import EmpLogin from "./Employee/Containers/EmpLogin";
-// import MedicalRecordTreatment from "./Employee/Containers/MedicalRecordTreatment";
-
-
-
 import Home from "./Home";
 import Login from "./Patients/Containers/Login";
 import ManagePatientRecord from "./Patients/Containers/ManagePatientRecord";
 import ProfilePatient from "./Patients/Containers/PatientProfile";
-// import ProfilePatient from "./containers/patientProfile";
-
-// import EmpLogin from "./Employees/Containers/EmpLogin";
-import EmpTest from "./Employees/Containers/EmpTest";
-import Registration from "./Employees/Containers/Registration";
-// import PatientTreatment from "./Employees/Containers/PatientTreatment";
-// import MedicalRecordTreatment from "./Employees/Containers/MedicalRecordTreatment";
-import apiDoc from "./Employees/Components/apiDoc"
-
-import APIHome from "./APIdata/Containers/APIHome"
-// import NavbarQueses from "./Employees/Components/NavbarQueses"
 import apiDocument from "./Employees/Components/apiDoc"
-// import Testt from "./Employees/Components/Test"
-// import DemoExample from "./containers/demoExample"
+import EmpTest from "./Employees/Containers/EmpTest";
 
 import 'semantic-ui-css/semantic.min.css';
 import 'react-datepicker/dist/react-datepicker.css';
@@ -48,26 +24,13 @@ ReactDOM.render(
       <Route component={scrollToTop} />
       <Route exact path="/" component={Home} />
       {/* patient */}
-      {/* <Route path="/signin" component={Login} />
+      <Route path="/signin" component={Login} />
       <Route path="/signup" component={ManagePatientRecord} />
       <Route path="/profile" component={ProfilePatient} />
-      {/* <Route path="/editProfile" component={EditProfile} /> */}
 
-      {/* emp */}
+      {/* vender */}
       <Route path="/empTest" component={EmpTest} />
-      {/* <Route path="/api" component={DemoExample} /> */}
-      
-      {/* <Route path="/signinForEmployee" component={EmpLogin} /> */}
-      <Route path="/Registration" component={Registration} /> 
-      {/* <Route path="/MedicalRecordTreatment" component={MedicalRecordTreatment} />  */}
-      {/* <Route path="/employeeSegment" component={EmployeeSegment} /> */}
-      {/* <Route path="/patientTreatment" component={PatientTreatment} /> */}
-
-      {/* <Route path="/stamp" component={Stamp} />
-      <Route path="/testMedicalRecord" component={MedicalRecordTreatment} />
-      <Route path="/test" component={NavbarQueses} /> */}
       <Route path="/apiDoc" component={apiDocument} />
-      {/* <Route path="/test2" component={Testt} /> */}
     </div>
   </Router>,
   document.getElementById("root")
