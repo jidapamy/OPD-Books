c5fa43febf6099cad6ebb73b1512962812f0ffaf
Surakiti <asia_like_za@hotmail.com>
2018-09-28 06:31:31 +0700

    [UPDATE] Edit API Contract

--- AppProjects/src/Lib/Axois.js
+++ AppProjects/src/Lib/Axois.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-// axios.defaults.baseURL = 'http://api.opdbooks.tk'
-axios.defaults.baseURL = 'http://localhost:3015'
+axios.defaults.baseURL = 'http://api.opdbooks.tk'
+// axios.defaults.baseURL = 'http://localhost:3015'
 
 export default axios;
