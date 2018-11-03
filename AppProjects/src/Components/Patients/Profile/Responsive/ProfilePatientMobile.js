b7fdaf665586dc103b393d48e3e51657498d99e1
Surakiti <asia_like_za@hotmail.com>
2018-11-04 05:00:27 +0700

    Update AppProjects/src/Components/Patients/Profile/Responsive/ProfilePatientMobile.js

--- AppProjects/src/Components/Patients/Profile/Responsive/ProfilePatientMobile.js
+++ AppProjects/src/Components/Patients/Profile/Responsive/ProfilePatientMobile.js
@@ -7,7 +7,7 @@ import { groupInfoPatientField, patientField } from "../../../../Static/Data/Fie
 import { color } from "../../../../Static/Data/ColorPattern"
 
 const BGMobiles = styled(Segment) `
-  background: url('https://i.pinimg.com/originals/6d/35/4a/6d354a52bfffd0aa61fe58ec6c46a956.png') !important;
+  background: url('https://www.pcrevue.sk/files/photo/2018-04/42366/40c02e/bg.jpg') !important;
   background-size: 100% 100%;
 `
 const labelInfo = {
@@ -46,17 +46,18 @@ export default class ProfilePatientMobile extends React.Component {
                             <Icon name='setting' onClick={() => this.props.setField("showEditPage" ,true)}></Icon>
                     </Grid.Column>
                 </Grid>
+                {/* https://react.semantic-ui.com/images/avatar/large/nan.jpg */}
                 <Image bordered={true} style={{ borderColor: 'white', borderWidth: '3px' }} src='https://react.semantic-ui.com/images/avatar/small/matthew.png' size='small' circular centered />
                 <Statistic size='mini'>
-                    <Header as="h2">
+                    <Header as="h2" style={{ color: '#31A5BA' }}>
                         {this.props.patient.nametitle} {this.props.patient[patientField.firstname.variable]} {this.props.patient[patientField.lastname.variable]}
                     </Header>
                     <Header.Subheader>
-                        <span style={{ color: color.teal }}>{patientField.citizenId.label} : </span>
+                        <span style={{ color: '#F4D03F' }}>{patientField.citizenId.label} : </span>
                         {this.props.patient[patientField.citizenId.variable]}
                     </Header.Subheader>
                     <Header.Subheader>
-                        <span style={{ color: color.teal }}>{patientField.email.label} : </span>
+                        <span style={{ color: '#F4D03F' }}>{patientField.email.label} : </span>
                         {this.props.patient[patientField.email.variable]}
                     </Header.Subheader>
                     {/* <Statistic.Label style={{ fontSize: '1.1em' }} inverted>{this.props.patient.nametitle} {this.props.patient.firstname} {this.props.patient.lastname}</Statistic.Label>
