268ed4288f5b68244cababd4ee29f0a0b43216b8
Surakiti <asia_like_za@hotmail.com>
2018-11-18 19:32:30 +0700

    [UPDATE] Deposit carch sms

--- ApiService/App/Lib/Nexmo.js
+++ ApiService/App/Lib/Nexmo.js
@@ -1,6 +1,6 @@
 const Nexmo = require('nexmo');
 const nexmo = new Nexmo({
-    apiKey: "7a703253",
-    apiSecret: "nnYoeVMC1O4E8yWq"
+    apiKey: "b8a8a070",
+    apiSecret: "sseiQQrkDaaT644k"
 });
 module.exports = nexmo
