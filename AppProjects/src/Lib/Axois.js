65ccec2ec5f5fe0cdb611c14f7fde8948bf37c68
Jidapa Sikaphan <jidapa40@hotmail.com>
2018-11-19 01:56:48 +0700

    [update] axios api blockchain

--- AppProjects/src/Lib/Axois.js
+++ AppProjects/src/Lib/Axois.js
@@ -1,6 +1,3 @@
 import axios from 'axios'
-// axios.defaults.baseURL = 'http://api.opdbooks.tk'
-axios.defaults.baseURL = 'http://localhost:3015'
-// axios.defaults.baseURL = 'http://18.136.124.127:3015/'
-
+axios.defaults.baseURL = 'https://api.opdbooks.tk'
 export default axios;
