8fa01eb95c45af9269a3d31382ee0d7270bf7f31
Surakiti <asia_like_za@hotmail.com>
2018-12-15 23:27:34 +0700

    [UPDATE] Add Config DB

--- AppProjects/src/Lib/Axois.js
+++ AppProjects/src/Lib/Axois.js
@@ -1,3 +1,5 @@
 import axios from 'axios'
-axios.defaults.baseURL = 'https://api.opdbooks.tk'
+// axios.defaults.baseURL = 'https://api.opdbooks.tk'
+axios.defaults.baseURL = 'http://localhost:3015/'
+
 export default axios;
